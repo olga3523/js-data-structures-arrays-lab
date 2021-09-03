59547d863f26304284eec367e0a1a63eca45b8fb
OLGA <olichka23.85@gmail.com>
2021-09-03 12:42:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+let append = [
+    "Milo",
+    "Otis",
+    "Garfield",
+    "Odie"
+];
+
+let prepend = [
+    "Odie",
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+
+let removeLast = [
+    "Milo"  , "Otis"
+];
+
+let removeFirst = [
+    "Otis"  , "Garfield"
+];
